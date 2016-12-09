222474a4c1562ca6a57f7fd80b0ef1e312f118cd
shekodn <sdiazn19@gmail.com>
2016-12-09 12:22:33 -0600

    first commit

---
+++ What's the wather/What's the wather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  What's the wather
+//
+//  Created by Sergio Díaz Navarro on 12/9/16.
+//  Copyright © 2016 Sergio Díaz Navarro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ What's the wather/What's the watherTests/What_s_the_watherTests.swift
@@ -0,0 +1,36 @@
+//
+//  What_s_the_watherTests.swift
+//  What's the watherTests
+//
+//  Created by Sergio Díaz Navarro on 12/9/16.
+//  Copyright © 2016 Sergio Díaz Navarro. All rights reserved.
+//
+
+import XCTest
+@testable import What_s_the_wather
+
+class What_s_the_watherTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
